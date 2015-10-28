5dbc412365f05ec8e9a6f25e3307c31cf058c9f6
Bob Ziroll <elderziroll@gmail.com>
2015-10-28 12:03:32 -0600

    Small refactor

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ var app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(function (req, res, next) {
     if (req.headers.origin) {
