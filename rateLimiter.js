c18625b4be4e8f380a68e17fd1bea6d0fdbd8b41
Bob Ziroll <elderziroll@gmail.com>
2018-02-23 17:03:11 -0700

    Modify rate limiter

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,8 +1,8 @@
 const RateLimit = require('express-rate-limit');
 
 const limiter = new RateLimit({
-    windowMs: 15*60*1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 30*1000, // 30 seconds
+    max: 1000, // limit each IP to 1000 requests per windowMs
     delayMs: 0 // disable delaying - full speed until the max limit is reached
 });
 
