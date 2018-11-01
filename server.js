12d360c15705eb5a95500c347bc3724ad97da4de
Bob Ziroll <elderziroll@gmail.com>
2018-11-01 14:53:58 -0600

    Remove extra stuff

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    if (req.headers.origin) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Authorization");
-        res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-        if (req.method === "OPTIONS") return res.send(200);
-    }
-    next();
-});
-
 mongoose.connect("mongodb://localhost:27017/api",
     { useNewUrlParser: true },
     (err) => {
