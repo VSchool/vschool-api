6c6231664cfbc00989c1d0d818d78c155ee95128
Bob Ziroll <elderziroll@gmail.com>
2018-11-01 12:37:53 -0600

    Added json files directly to server, server serves them up

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path")
 const port = process.env.PORT || 9191;
 
 app.use(morgan("dev"));
@@ -28,6 +29,13 @@ mongoose.connect("mongodb://localhost:27017/api",
 );
 
 app.use("/:sessionId/todo", require("./routes/todoRoutes"));
+app.get("/pokemon", (req, res) => {
+    return res.sendFile(path.join(__dirname, "static", "pokemon.json"))
+})
+
+app.get("/hitlist", (req, res) => {
+    return res.sendFile(path.join(__dirname, "static", "hitlist.json"))
+})
 
 app.use((err, req, res, next) => {
     console.error(err);
